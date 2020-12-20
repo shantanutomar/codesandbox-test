048e8263c4d9c0eaf35351fe502bebae8b74376c
Shantanu Tomar <shantanu.tomar89@gmail.com>
2020-12-20 17:18:32 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+import Header from "./components/Header";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <Header />
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
